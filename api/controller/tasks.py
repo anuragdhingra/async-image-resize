03aa2f39dc7a62bb0a8523acaa646ca2b75ca31d
subodh101 <subodh.pushkar@gmail.com>
2019-09-04 21:59:56 +0900

    reformat using black (https://black.readthedocs.io/en/stable/installation_and_usage.html)

--- api/controller/tasks.py
+++ api/controller/tasks.py
@@ -8,28 +8,29 @@ import os
 logger = logging.getLogger()
 celery = Celery(__name__, autofinalize=False)
 
+
 @celery.task()
-def async_resize(file_uuid,img_string):
+def async_resize(file_uuid, img_string):
     logger.info("Started resizing...")
 
-    img_str = img_string.split("base64,",1)[-1]
+    img_str = img_string.split("base64,", 1)[-1]
     temp_filename = file_uuid + "_temp.jpg"
 
     with open(temp_filename, "wb") as fh:
         fh.write(base64.decodebytes(img_str.encode()))
-    img = Image.open(temp_filename) 
+    img = Image.open(temp_filename)
 
-    #for .png images where there is no alpha
-    rgb_img = img.convert('RGB')
+    # for .png images where there is no alpha
+    rgb_img = img.convert("RGB")
     new_filename = file_uuid + ".jpg"
-    new_img = rgb_img.resize((100,100), Image.ANTIALIAS)
-    new_img.save(new_filename,'JPEG',optimize=True, quality=100)
+    new_img = rgb_img.resize((100, 100), Image.ANTIALIAS)
+    new_img.save(new_filename, "JPEG", optimize=True, quality=100)
 
     logger.info("Completed resizing...")
 
-    #remove the temporary file
+    # remove the temporary file
     try:
         os.remove(temp_filename)
     except OSError:
         pass
-    return "Saved as: " + new_filename 
+    return "Saved as: " + new_filename
--- worker.py
+++ worker.py
@@ -1,2 +1,3 @@
 from api import create_worker
+
 celery = create_worker()
